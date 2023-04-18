062fcf2a03cfcbfb72701cbbec51c1971634a2b4
letrunks <trungbts3@gmail.com>
2023-04-18 13:44:59 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Button from "@components/atoms/Button";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-green-600">Test thành công</h1>
+      <h1 className="text-3xl font-bold text-green-600">Lê Văn Trung</h1>
       <Button />
     </div>
   );
