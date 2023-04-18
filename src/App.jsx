48ee6499ebb79996f80efea8a0b09cc2fdd89574
Trunks <Trungthekop123@gmail.com>
2023-04-18 13:34:37 +0700

    Push code base structure react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unreachable */
+import React from "react";
+import Button from "@components/atoms/Button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-green-600">Xin Trung 2001</h1>
+      <Button />
+    </div>
+  );
+};
+export default App;
