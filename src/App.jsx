5cd0b4ee81bc63a710ca3a3ea5c9153f1f3e7b4c
letrunks <trungbts3@gmail.com>
2023-04-18 13:42:16 +0700

    alias

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Button from "@components/atoms/Button";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-green-600">Xin Trung 2001</h1>
+      <h1 className="text-3xl font-bold text-green-600">Test thành công Alias</h1>
       <Button />
     </div>
   );
